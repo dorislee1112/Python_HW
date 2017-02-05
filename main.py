dbb734be7f052373fbfd52ea529bc79be397ffce
Doris Lee <dorislee1112@gmail.com>
2017-02-05 21:04:13 +0800

    firt commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import logging
+import sys
+from datetime import datetime
+from numpy import array
+from PIL import Image
+import cv2
+
+import tensorflow as tf
+
+'''
+1. import `Video` and `save_video` from the correct module of package "styler"
+'''
+from styler.video import Video
+from styler.utils import save_video
+
+
+model_file = 'data/vg-30.pb'
+model_name = 'vg-30'
+input_file = 'input/nadal.mp4'
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s %(levelname)s:%(message)s',
+    level=logging.INFO,
+    datefmt='%I:%M:%S')
+
+
+def main():
+
+    with open(model_file, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def)
+        graph = tf.get_default_graph()
+
+    with tf.Session(config=tf.ConfigProto(
+            intra_op_parallelism_threads=4)) as session:
+
+        logging.info("Initializing graph")
+        session.run(tf.global_variables_initializer())
+
+        image = graph.get_tensor_by_name("import/%s/image_in:0" % model_name)
+        out = graph.get_tensor_by_name("import/%s/output:0" % model_name)
+        
+        shape = image.get_shape().as_list()
+        
+
+        '''
+        2. set the `path` to  your input
+        '''
+        #print (shape[0])
+        with Video(input_file) as v:
+            frames = v.read_frames(image_h=shape[1], image_w=shape[2])
+
+        logging.info("Processing image")
+        start_time = datetime.now()
+        
+        #shape2 = frames.get_shape().as_list()
+        
+        '''
+        3. Write a list comprehension to iterate through all frames,
+           and make it be processed by Tensorflow.
+        '''
+        #Sprint (shape2)
+        #processed = []
+        processed = [
+            session.run(out, feed_dict={image: [frame]}) for frame in frames
+        ]
+
+        '''
+        i=0
+        for frame in frames:
+            #frame = array(frame).reshape(1, frame.shape[0], frame.shape[1], frame.shape[2])
+            #np.expand_dims(frame, axis=0)
+            print (str(i))
+            mix_img=session.run(out, feed_dict={image: frame})
+            #s='result'+str(i)+'.jpeg'
+            #print(s)
+            #im = Image.fromarray(out)
+            #im.save(s)
+            #cv2.imwrite(s, out)
+            processed.append(mix_img)
+            i=i+1
+        '''
+        #]
+
+        #print ('end')
+
+        '''
+        4. Pass the results as a argument into function
+        '''
+        save_video('result.mp4',
+                   fps=30, h=shape[1], w=shape[2],
+                   frames=processed)
+
+        logging.info("Processing took %f" % (
+            (datetime.now() - start_time).total_seconds()))
+        logging.info("Done")
+
+
+if __name__ == '__main__':
+    main()
---
+++ styler/video.py
@@ -0,0 +1,52 @@
+import cv2
+
+from styler.utils import resize
+import numpy as np
+
+class Video:
+
+    def __init__(self, path):
+        self.path = path
+        self.cap = cv2.VideoCapture(self.path)
+        self.frames = []
+
+    def __enter__(self):
+        if not self.cap.isOpened():
+            raise Exception('Cannot open video: {}'.format(self.path))
+        return self
+
+    def __len__(self):
+        return len(self.frames)
+
+    def read_frames(self, image_h, image_w):
+        '''
+        5.
+         - Read video frames from `self.cap` and collect frames into list
+         - Apply `resize()` on each frame before add it to list
+         - Also assign frames to "self" object
+         - Return your results
+        '''
+        frames = []
+        # 5-1 /5-2 Read video and collect them
+
+        while (self.cap.isOpened()):
+            ret, getframe=self.cap.read()
+            if ret==True:
+                #print (getframe.shape)
+                resize_frame=resize(getframe,image_h,image_w)
+                #shape = resize_frame.get_shape().as_list()
+                #print(shape)
+                #print (resize_frame.shape[2])
+                #resize_frame = array(resize_frame).reshape(1, image_w, image_h, 3)
+                #np.expand_dims(img, axis=0)
+                frames.append(resize_frame)
+                #frames.append(getframe)
+            else:
+                break
+            
+        self.frames=frames
+        #self.frames = frames  # 5-3 let object have the result
+        return self.frames  # return your results
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cap.release()
